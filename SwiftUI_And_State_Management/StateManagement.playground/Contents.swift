a96d0b2541abbfb1e0777d1dd7cdf5ade767b59a
pptaa <chobo05@naver.com>
2024-07-31 00:35:19 +0900

    ✨ SwiftUI And State Management Part.1

--- SwiftUI_And_State_Management/StateManagement.playground/Contents.swift
+++ SwiftUI_And_State_Management/StateManagement.playground/Contents.swift
@@ -1,3 +1,86 @@
-import UIKit
+import SwiftUI
 
-var greeting = "Hello, playground"
+struct ContentView: View {
+  @ObservedObject var state: AppState
+  var body: some View {
+    NavigationView {
+      List {
+        NavigationLink(
+          destination: CounterView(state: self.state)
+        ) {
+          Text("Counter demo")
+        }
+        NavigationLink(
+          destination: EmptyView()
+        ) {
+          Text("Favorite primes")
+        }
+      }
+      .navigationTitle("State Management")
+    }
+  }
+}
+
+import Combine
+class AppState: ObservableObject {
+  @Published var count = 0
+  
+}
+
+struct CounterView: View {
+  @ObservedObject var state: AppState
+  
+  var body: some View {
+    VStack {
+      HStack {
+        Button(
+          action: {
+            self.state.count -= 1
+          },
+          label: {
+            Text("-")
+          }
+        )
+        Text("\(self.state.count)")
+        Button(
+          action: {
+            self.state.count += 1
+          },
+          label: {
+            Text("+")
+          }
+        )
+      }
+      
+      Button(
+        action: {},
+        label: {
+          Text("Is this prime?")
+        }
+      )
+      Button(
+        action: {},
+        label: {
+          Text("What is the \(ordinal(self.state.count)) prime?")
+        }
+      )
+    }
+    .font(.title)
+    .navigationTitle("Counter Demo")
+  }
+}
+
+private func ordinal(_ n: Int) -> String {
+  let formatter = NumberFormatter()
+  formatter.numberStyle = .ordinal
+  return formatter.string(for: n) ?? ""
+}
+
+
+import PlaygroundSupport
+
+PlaygroundPage.current.liveView = UIHostingController(
+  rootView: ContentView(
+    state: AppState()
+  )
+)
